f4cd580ee913a4cfc4bc6fbc272a77f7974d9c0c
Yotam Moss <yotam.mos@gmail.com>
2021-05-29 22:18:03 -0400

    moving to gin

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # golang-notes-app
 
-Simple notes app in Go, using the Martini framework https://github.com/go-martini/martini and in memory database https://github.com/hashicorp/go-memdb
+Simple notes app in Go, using the Gin framework https://github.com/gin-gonic/gin and in memory database https://github.com/hashicorp/go-memdb
--- main.go
+++ main.go
@@ -2,86 +2,88 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/encoder"
+	"github.com/gin-gonic/gin"
 	"notes-app/model"
 	"notes-app/service"
-	"strconv"
 )
 import "net/http"
 import _ "github.com/hashicorp/go-memdb"
 
-func hello() (int, string) {
-	return http.StatusOK, "Hello world!"
+func buildGinResponse(c *gin.Context, code int, obj interface{}) {
+	c.JSON(code, obj)
 }
 
-func notFound() (int, string) {
-	return http.StatusNotFound, "resource not found"
+func listNotes(notesService service.NotesService) (code int, obj interface{}) {
+	err, noteIds := notesService.ListNotes()
+	if err != nil {
+		return err.StatusCode, gin.H{
+			"message": err.Message,
+		}
+	}
+	if noteIds == nil || len(noteIds) == 0 {
+		return http.StatusNotFound, gin.H{
+			"message": "resource not found",
+		}
+	}
+	return http.StatusOK, noteIds
+}
+
+func createNote(c *gin.Context, notesService service.NotesService) (code int, obj interface{}) {
+	data, err := c.GetRawData()
+	if err != nil {
+		return http.StatusBadRequest, gin.H{
+			"message": "failed getting request body",
+		}
+	}
+
+	var request model.CreateNoteRequest
+	err = json.Unmarshal(data, &request)
+	if err != nil {
+		return http.StatusBadRequest, gin.H{
+			"message": "invalid request body",
+		}
+	}
+
+	response := notesService.CreateNote(request)
+	return response.StatusCode, response.Message
+}
+
+func describeNote(c *gin.Context, notesService service.NotesService) (code int, obj interface{}) {
+	id := c.Param("id")
+	errorResponse, note := notesService.DescribeNote(id)
+	if errorResponse != nil {
+		return errorResponse.StatusCode, gin.H{
+			"message": errorResponse.Message,
+		}
+	}
+
+	return http.StatusOK, note
+}
+
+func startGin(notesService service.NotesService) {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		buildGinResponse(c, http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/notes", func(c *gin.Context) {
+		code, data := listNotes(notesService)
+		buildGinResponse(c, code, data)
+	})
+	r.PUT("/notes", func(c *gin.Context) {
+		code, data := createNote(c, notesService)
+		buildGinResponse(c, code, data)
+	})
+	r.GET("/notes/:id", func(c *gin.Context) {
+		code, data := describeNote(c, notesService)
+		buildGinResponse(c, code, data)
+	})
+	r.Run()
 }
 
 func main() {
 	var notesService service.NotesService
 	notesService = service.CreateService()
-	m := martini.Classic()
-
-	m.Use(func(c martini.Context, w http.ResponseWriter, r *http.Request) {
-		// Use indentations. &pretty=1
-		pretty, _ := strconv.ParseBool(r.FormValue("pretty"))
-		// Use null instead of empty object for json &null=1
-		null, _ := strconv.ParseBool(r.FormValue("null"))
-		// Some content negotiation
-		switch r.Header.Get("Content-Type") {
-			case "application/xml":
-				c.MapTo(encoder.XmlEncoder{PrettyPrint: pretty}, (*encoder.Encoder)(nil))
-				w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-			default:
-				c.MapTo(encoder.JsonEncoder{PrettyPrint: pretty, PrintNull: null}, (*encoder.Encoder)(nil))
-				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		}
-	})
-	m.Get("/", hello)
-	m.Get("/notes", func(enc encoder.Encoder) (int, []byte) {
-		res, noteIds := notesService.ListNotes()
-		if res != nil {
-			return res.StatusCode, encoder.Must(enc.Encode(model.Message{
-				Content: res.Message,
-			}))
-		}
-
-		if noteIds == nil || len(noteIds) == 0 {
-			return http.StatusNotFound, encoder.Must(enc.Encode(model.Message{
-				Content: "resource not found",
-			}))
-		}
-
-		ids, marshalingError := json.Marshal(noteIds)
-		if marshalingError != nil {
-			return http.StatusBadRequest, encoder.Must(enc.Encode(model.Message{
-				Content: "failed building json",
-			}))
-		}
-		return http.StatusOK, ids
-	})
-	m.Get("/notes/:id", func(c martini.Context, params martini.Params) (int, string) {
-		res, note := notesService.DescribeNote(params["id"])
-		if res != nil {
-			return res.StatusCode, res.Message
-		}
-		jsonNote, marshalingError := json.Marshal(note)
-		if marshalingError != nil {
-			return http.StatusBadRequest, "failed building json"
-		}
-		return http.StatusOK, string(encoder.Must(jsonNote, nil))
-	})
-	m.Put("/notes", func(req *http.Request) (int, string) {
-		var request model.CreateNoteRequest
-		err := json.NewDecoder(req.Body).Decode(&request)
-		if err != nil {
-			return http.StatusBadRequest, err.Error()
-		}
-		res := notesService.CreateNote(request)
-		return res.StatusCode, res.Message
-	})
-	m.NotFound(notFound)
-	m.Run()
+	startGin(notesService)
 }
